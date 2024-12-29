cb769b0aec831a4d0e2da1966adbf7bebc597472
Rahaf-Arabawi <rahafaqrabawi2003@gmail.com>
2024-12-29 21:37:38 +0300

    javascript1

---
+++ style1.js
@@ -0,0 +1,30 @@
+var x=1000,y=500;
+
+document.write('Cash flow ratio=')
+document.write(x/y + "<br>")
+
+document.write('Net income=')
+document.write(x-y + "<br>")
+
+document.write('Total assets=')
+document.write(1000+500 + "<br>")
+
+document.write('Net income (using profit margin and sales)=')
+document.write(1000*500 + "<br>")
+
+var a=7,b=9,m=2;
+
+document.write('Average=')
+document.write((a+b+m)/3  + "<br>")
+
+var m=150;
+
+document.write('Discount=')
+document.write(m-(m)*0.30  + "<br>")
+
+var age=20;
+document.write((age > 18 && age < 30 ) + "<br>")
+
+document.write( Math . pow(2,3) + "<br>")
+
+document.write(10 % 4)
